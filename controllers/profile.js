cd30a130dc446c34f8b58945e01a8060a40d5a4f
Colin Smith <88501518+colinksmith@users.noreply.github.com>
2023-01-23 11:57:19 -0500

    profile pages working

--- controllers/profile.js
+++ controllers/profile.js
@@ -9,11 +9,18 @@ module.exports = {
     try {
       if (req.params.userId){
         console.log(req.params.userId)
+        const profile = await User.findById(req.params.userId)
+        const posts = await Post.find({ user: profile.id });
+        res.render("profile.ejs", { posts: posts, user: profile });
+      } else if (req.user){
+        const posts = await Post.find({ user: req.user.id });
+        res.render("profile.ejs", { posts: posts, user: req.user });
+      } else {
+        res.redirect("/");
       }
-      const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
+      res.redirect("/");
     }
   },
 }
--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,7 @@ const profileController = require("../controllers/profile");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
-// router.get("/", ensureAuth, profileController.getProfile);
+router.get("/", ensureAuth, profileController.getProfile);
 router.get("/:userId", profileController.getProfile);
 
 module.exports = router;
